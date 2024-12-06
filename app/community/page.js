5f1dc2b3087bcb40e22972f20215c458f806d95f
Yazdanparast <sajad.yazdan@fdmgroup.com>
2024-12-06 02:55:07 -0500

    Build community and meals pages

--- app/community/page.js
+++ app/community/page.js
@@ -1,7 +1,40 @@
+import Image from "next/image";
+
+import mealIcon from "@/assets/icons/meal.png";
+import communityIcon from "@/assets/icons/community.png";
+import eventsIcon from "@/assets/icons/events.png";
+import classes from "./page.module.css";
+
 export default function CommunityPage() {
   return (
-    <main>
-      <h1>Community Page!</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          A shared love for: <span className={classes.highlight}>Food</span>
+        </h1>
+        <p>Be part of our community and showcase your favorite recipes!</p>
+      </header>
+      <main className={classes.main}>
+        <h2>Community Benefits</h2>
+
+        <ul className={classes.perks}>
+          <li>
+            <Image src={mealIcon} alt="An appetizing dish" />
+            <p>Explore and share amazing recipes</p>
+          </li>
+          <li>
+            <Image
+              src={communityIcon}
+              alt="A group of people enjoying cooking"
+            />
+            <p>Connect with friends and fellow enthusiasts</p>
+          </li>
+          <li>
+            <Image src={eventsIcon} alt="A lively cooking event" />
+            <p>Join exclusive culinary events</p>
+          </li>
+        </ul>
+      </main>
+    </>
   );
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,23 @@
+import { calculateSizeAdjustValues } from "next/dist/server/font-utils";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Yummy meals, created <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Select the recipe you like and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share the recipe you like</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        {" "}
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
